99d817e9ed623c3eb2a9994ba0e8f974ac24081f
Augusto Martinez <acm_asd@hotmail.com>
2020-05-04 10:47:42 -0600

    updates

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,9 +15,11 @@ class HttpsProtocol
      */
   public function handle($request, Closure $next)
   {
-    if (!$request->secure() && env('APP_ENV') === 'prod') {
-      return redirect()->secure($request->getRequestUri());
-    }
+    if (!$request->secure() && env('APP_ENV') === 'production') {
+
+            return redirect()->secure($request->getRequestUri());
+
+        }
       return $next($request); 
   }
 }
